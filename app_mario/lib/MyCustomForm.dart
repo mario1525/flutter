f4d051d0544bc33032a28fb8552dcb8c0206e7a7
mario1525 <mbeltran4892@gmail.com>
2022-03-07 15:50:55 -0500

    base de la pagina de login

---
+++ app_mario/lib/MyCustomForm.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class MyCustomForm extends StatefulWidget {
+  const MyCustomForm({Key? key}) : super(key: key);
+
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.center,
+            margin: new EdgeInsets.symmetric(vertical: 120.0),
+          ),
+          TextFormField(
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: 'ingrese su correo',
+            ),
+            // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 20.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+                  );
+                }
+              },
+              child: const Text('login'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- app_mario/lib/main.dart
+++ app_mario/lib/main.dart
@@ -1,55 +1,21 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'MyCustomForm.dart';
 
 void main() {
   runApp(myapp());
 }
 
 class myapp extends StatelessWidget {
-  const myapp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return const MaterialApp(
-      title: 'primera pagina',
-      home: RandomWords(),
-    );
-  }
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Startup Name Generator'),
-      ),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return const Divider(); /*2*/
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return ListTile(
-            title: Text(
-              _suggestions[index].asPascalCase,
-              style: _biggerFont,
-            ),
-          );
-        },
+    return MaterialApp(
+      title: 'login',
+      home: Scaffold(
+        body: new Stack(
+          children: [MyCustomForm()],
+        ),
+        backgroundColor: Color.fromARGB(255, 3, 39, 68),
       ),
     );
   }
-}
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
 }
