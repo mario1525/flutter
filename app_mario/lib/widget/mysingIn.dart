ebd77dc045dd38ef70c0b3962d5ec11d29ea499a
mario1525 <mbeltran4892@gmail.com>
2022-03-30 22:26:22 -0500

    posicionando los textos

--- app_mario/lib/widget/mysingIn.dart
+++ app_mario/lib/widget/mysingIn.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'package:app_mario/widget/backgraund.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -12,60 +13,75 @@ class MyCustomForm extends StatefulWidget {
 class MyCustom extends State<MyCustomForm> {
   @override
   Widget build(BuildContext context) {
-    return Form(
-      child: Center(
-        child: Stack(
-          children: <Widget>[
-            backgraund(),
-            //boton de navegacion
-            Positioned(
-              bottom: 750,
-              left: -20,
-              child: CupertinoButton.filled(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: const Icon(Icons.arrow_back),
-              ),
-            ),
-            Positioned(
-              child: _correo(),
-            ),
-            SizedBox(
-              height: 200,
-            ),
-            Positioned(
-              child: _contra(),
-            ),
-          ],
+    return Stack(
+      children: <Widget>[
+        backgraund(),
+        //boton de navegacion
+        Positioned(
+          bottom: 780,
+          left: -40,
+          child: CupertinoButton.filled(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: const Icon(Icons.arrow_back),
+          ),
         ),
-      ),
+        Positioned(
+          left: 40,
+          bottom: 300,
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 2),
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: 280,
+            color: Color.fromARGB(255, 215, 216, 216),
+          ),
+        ),
+        Positioned(
+          child: form(),
+        ),
+      ],
     );
   }
 }
 
-Container _correo() {
-  return Container(
-    decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        border: Border.all(color: Colors.black)),
-    padding: const EdgeInsets.symmetric(horizontal: 15),
-    margin: const EdgeInsets.symmetric(horizontal: 15),
-    child: CupertinoTextField(
-      style: const TextStyle(fontSize: 20),
-    ),
-  );
-}
-
-Container _contra() {
-  return Container(
-    decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(5),
-        border: Border.all(color: Colors.black)),
-    padding: const EdgeInsets.symmetric(horizontal: 15),
-    margin: const EdgeInsets.symmetric(horizontal: 15),
-    child: CupertinoTextField(
-      style: const TextStyle(fontSize: 20),
-    ),
-  );
+class form extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 400.0,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.7,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            child: CupertinoTextField(
+              style: const TextStyle(fontSize: 22),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.7,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            child: CupertinoTextField(
+              style: const TextStyle(fontSize: 22),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
--- app_mario/lib/widget/mysingUp.dart
+++ app_mario/lib/widget/mysingUp.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'backgraund.dart';
+import 'package:flutter/material.dart';
 
 class mysingUp extends StatefulWidget {
   mysingUp({Key? key}) : super(key: key);
@@ -15,16 +16,96 @@ class mysinUpapp extends State<mysingUp> {
         backgraund(),
         //boton de navegacion
         Positioned(
-          bottom: 375,
-          left: 125,
+          bottom: 780,
+          left: -40,
           child: CupertinoButton.filled(
             onPressed: () {
               Navigator.pop(context);
             },
-            child: const Text('home'),
+            child: const Icon(Icons.arrow_back),
           ),
-        )
+        ),
+        Positioned(
+          left: 40,
+          bottom: 220,
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 2),
+            width: MediaQuery.of(context).size.width * 0.8,
+            height: 380,
+            color: Color.fromARGB(255, 215, 216, 216),
+          ),
+        ),
+        Positioned(child: form()),
       ],
+    );
+  }
+}
+
+class form extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: 400.0,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            child: CupertinoTextField(
+              style: const TextStyle(fontSize: 23),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            child: CupertinoTextField(
+              style: const TextStyle(fontSize: 22),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            child: CupertinoTextField(
+              style: const TextStyle(fontSize: 22),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width * 0.8,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            margin: const EdgeInsets.symmetric(horizontal: 15),
+            child: CupertinoTextField(
+              style: const TextStyle(fontSize: 22),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
